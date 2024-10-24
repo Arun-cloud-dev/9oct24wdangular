44d2a5194eb6b5192b5117380b11a765dddbff2d
Arun raj <riderarunraj@live.com>
2024-10-24 20:35:58 +0530

    update ng if and ng for

--- Angular02/src/app/app.component.ts
+++ Angular02/src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component  } from '@angular/core';
+import { NgFor , NgIf } from '@angular/common';
+
+
+
+
+
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-user';
@@ -8,13 +14,13 @@ import { TaskComponent } from './task/task.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TaskComponent],
+  imports: [HeaderComponent, UserComponent, TaskComponent, NgFor , NgIf ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId= 'u1'
+  selectedUserId?: string;
 
   get  selectedUser(){
     return this.users.find((user) => user.id === this.selectedUserId)!;
--- Angular02/src/app/user/user.component.ts
+++ Angular02/src/app/user/user.component.ts
@@ -1,38 +1,32 @@
-import { Component, EventEmitter, Input, Output, output } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// }
 
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
-
-
 export class UserComponent {
-  @Input ({required:true}) id!: string;
-  @Input({required:true}) avatar!: string;
-  @Input({required:true}) name!: string ;
-  @Output () select = new EventEmitter();   //<any data type>..... eg <string > etc 
+  @Input({ required: true }) user!: User;
+  @Output() select = new EventEmitter();
 
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
 
-  // select = output<string>();
-
-
-
-
-
-get imagePath (){
-  return 'assets/users/' + this.avatar
-}
-
-
-  onSelectUser(){
-    this.select.emit(this.id);
+  onSelectUser() {
+    this.select.emit(this.user.id);
   }
 }
-
-
-
